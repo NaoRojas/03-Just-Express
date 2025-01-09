4f04bc1b0e5e0d41540bf3b1c34f4cfbd0792267
naoreds <naorojash@gmail.com>
2025-01-08 19:01:51 -0600

    Add user validation middleware for all paths and methods

--- Module2_MiddlewareAndRendering/appUse.js
+++ Module2_MiddlewareAndRendering/appUse.js
@@ -10,8 +10,21 @@ function validateUser(req, res, next) {
   // 🐨 You call next() to move to the next middleware function
   next()
 }
+// 🐨 This will run validateUser on ALL paths, ALL methods!
+app.use('/', validateUser)
 
+// 🐨 This will run validateUser on /admin, ALL methods!
 app.use('/admin', validateUser)
+
+// 🐨 This will run validateUser on /, only on GET methods
+app.get('/', validateUser)
+// An it looks like this:
+// app.get('/', (req, res, next) => {
+//   res.locals.validated = true
+//   console.log("Validated Ran!")
+//   next()
+// })
+
 
 app.get('/', (req, res, next) => {
   res.send('<h1>Main Page</h1>')
