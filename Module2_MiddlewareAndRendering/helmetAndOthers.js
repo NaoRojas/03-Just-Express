3cf0209400bdeb028d69ad16db081a2730894474
naoreds <naorojash@gmail.com>
2025-01-08 22:47:58 -0600

    Add Helmet for enhanced security and update axios response handling

--- Module2_MiddlewareAndRendering/helmetAndOthers.js
+++ Module2_MiddlewareAndRendering/helmetAndOthers.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const helmet = require('helmet');
 const app = express()
 
+app.use(helmet())
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -8,9 +10,10 @@ app.use(express.urlencoded({ extended: false }))
 app.post('/axios', (req, res) => {
   console.log("Headers", req.headers)
   console.log("Body", req.body)
-  res.send('Hello World!')
+  res.json({ status: 'ok' })
 }
 )
 // 🐨 In the browser go to http://localhost:3000/axios.html
+// In PostMan, send a POST request to http://localhost:3000/axios
 
 app.listen(3000)
