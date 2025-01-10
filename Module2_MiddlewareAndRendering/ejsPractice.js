45ab1a2e9c8b7fc4736e1cb46e4b25200f97bdcc
naoreds <naorojash@gmail.com>
2025-01-10 17:48:47 -0600

    Add user validation middleware and enhance EJS view with additional data

--- Module2_MiddlewareAndRendering/ejsPractice.js
+++ Module2_MiddlewareAndRendering/ejsPractice.js
@@ -14,6 +14,13 @@ app.use(helmet({
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+function validateUser(req, res, next) {
+  res.locals.validated = true
+  console.log('validated ran!')
+  next()
+}
+
+app.use(validateUser)
 app.get('/', (req, res) => {
   // 🐨 In order to use res.render we need:
   // 1. We define a view engine (like mustache, ejs, handlebars, jade/pug, etc)
@@ -23,10 +30,13 @@ app.get('/', (req, res) => {
   //   b. The data we want to pass to that file
   // 4. Express uses the node module for our view engine and parses the file
   // 5. The final result of this proccess is a compiled product of the things the browser can read (HTML, CSS, JS)
-
   res.render('index',
     {
-      msg: 'Success!'
+      msg: 'Success!',
+      msg2: 'Hello World!',
+      html:
+        `<p>This is an HTML paragraph</p>
+        <p><strong>This is an HTML paragraph with strong tags</strong></p>`,
     }
   )
 })
